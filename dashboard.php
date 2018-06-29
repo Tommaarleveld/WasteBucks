660aeb567002dbf8ae9e99b804b47bb4173a3dc8
Tom Maarleveld <tom_maarleveld@live.nl>
2018-06-29 16:53:06 +0200

    Added dashboard and leaderboards where users can track their progress

---
+++ dashboard.php
@@ -0,0 +1,63 @@
+<?php ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>WasteBucks</title>
+<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+<link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.teal-red.min.css" />
+<link rel="stylesheet" href="build/stylesheets/index.css">
+<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Roboto:300,400,500,700" type="text/css">
+</head>
+
+<body>
+<!-- Header -->
+<div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+  <header class="mdl-layout__header">
+    <div class="mdl-layout__header-row">
+      <!-- Title -->
+      <span class="mdl-layout-title">Dashboard</span>
+      <!-- Add spacer, to align navigation to the right -->
+      <div class="mdl-layout-spacer"></div>
+    </div>
+  </header>
+  <?php include 'includes/header.php';?>
+
+  <!-- Page content -->
+  <main class="mdl-layout__content">
+    <div class="mdl-grid cyan-bg-element">
+    <div class="mdl-cell mdl-cell--2-col mdl-cell--4-col-phone"><span class="item-title center-text">Balance</span></div>
+    <div class="mdl-cell mdl-cell--2-col mdl-cell--1-col-phone mdl-cell--1-offset-phone"><img src="images/WasteBucksLogo.png"></div>
+    <div class="mdl-cell mdl-cell--2-col mdl-cell--2-col-phone"><p id="wb-ammount">1200</p></div>
+    </div>
+    <div class="mdl-grid no-bg-element">
+      <div class="mdl-cell mdl-cell--2-col mdl-cell--4-col-phone"><span class="item-title center-text">Statistics</span></div>
+      <div class="mdl-cell mdl-cell--2-col mdl-cell--4-col-phone"><p class="graph-title center-text">Waste count: <span class="blue-big" id="wc-number">322</span> items</p></div>
+      <div class="mdl-cell mdl-cell--2-col mdl-cell--4-col-phone">
+        <canvas id="myChart"></canvas>
+      </div>
+      <div class="mdl-cell mdl-cell--2-col mdl-cell--3-col-phone">
+        <ol id="statList">
+          <li>WasteBucks verzameld</li>
+          <li>Donaties aan goede doelen</li>
+          <li>Vuilbelasting voordeel</li>
+        </ol>
+      </div>
+      <div class="mdl-cell mdl-cell--2-col mdl-cell--1-col-phone">
+        <ul class="nobullet">
+          <li><div class="box wb-color" width="15px" height="20px"></div></li>
+          <li><div class="box cd-color" width="15px" height="20px"></div></li>
+          <li><div class="box tx-color" width="15px" height="20px"></div></li>
+        </ul>
+      </div>
+      </div>
+  </main>
+</div>
+
+<script src="javascript/material.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.min.js"></script>
+<script src="javascript/chart.js"></script>
+</body>
+</html>
---
+++ leaderboard.php
@@ -0,0 +1,309 @@
+<?php ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>WasteBucks</title>
+<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+<link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.teal-red.min.css" />
+<link rel="stylesheet" href="build/stylesheets/index.css">
+<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Roboto:300,400,500,700" type="text/css">
+<link href="https://fonts.googleapis.com/css?family=Raleway:400,300,600,800,900" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+<!-- Header -->
+<div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+  <header class="mdl-layout__header">
+    <div class="mdl-layout__header-row">
+      <!-- Title -->
+      <span class="mdl-layout-title">Leaderboards</span>
+      <!-- Add spacer, to align navigation to the right -->
+      <div class="mdl-layout-spacer"></div>
+      <a href="https://ctt.ec/c1LZf" target="_blank"> <i class="material-icons white">
+      share
+      </i></a>
+    </div>
+  </header>
+  <?php include 'includes/header.php';?>
+
+  <!-- Page content -->
+  <main class="mdl-layout__content">
+    <div class="mdl-grid">
+
+      <div class="full-width-card mdl-cell mdl-cell--2-col mdl-cell--4-col-phone">
+        <div class="gold-bg unset-height demo-card-square mdl-card mdl-shadow--2dp">
+          <div class="mdl-cell mdl-cell--2-col mdl-cell--4-col-phone">
+            <div class="mdl-grid">
+              <div class="mdl-cell mdl-cell--2-col mdl-cell--1-col-phone">
+                <img class="hunna-width" src="images/Avatar1.png">
+              </div>
+              <div class="mdl-cell mdl-cell--2-col mdl-cell--2-col-phone">
+                <div class="mdl-grid">
+                  <div class="mdl-cell mdl-cell--2-col mdl-cell--4-col-phone">
+                    <p class="name-highlight close-text">Karel de Korte</p>
+                  </div>
+                  <div class="mdl-cell mdl-cell--2-col mdl-cell--4-col-phone">
+                    <p class="close-text">Den Haag</p>
+                  </div>
+                
+              </div>
+            </div>
+
+            <div class="mdl-cell mdl-cell--2-col mdl-cell--1-col-phone">
+              <div class="mdl-grid">
+                <div class="mdl-cell mdl-cell--2-col mdl-cell--4-col-phone">
+                  <p class="name-highlight close-text">#1</p>
+                </div>
+                <div class="mdl-cell mdl-cell--2-col mdl-cell--4-col-phone">
+                   <p class="close-text">638</p>
+                </div>
+              </div>
+            </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="full-width-card mdl-cell mdl-cell--2-col mdl-cell--4-col-phone">
+        <div class="silver-bg unset-height demo-card-square mdl-card mdl-shadow--2dp">
+          <div class="mdl-cell mdl-cell--2-col mdl-cell--4-col-phone">
+            <div class="mdl-grid">
+              <div class="mdl-cell mdl-cell--2-col mdl-cell--1-col-phone">
+                <img class="hunna-width" src="images/Avatar2.png">
+              </div>
+              <div class="mdl-cell mdl-cell--2-col mdl-cell--2-col-phone">
+                <div class="mdl-grid">
+                  <div class="mdl-cell mdl-cell--2-col mdl-cell--4-col-phone">
+                    <p class="name-highlight close-text">Rajiv Rombout</p>
+                  </div>
+                  <div class="mdl-cell mdl-cell--2-col mdl-cell--4-col-phone">
+                    <p class="close-text">Groningen</p>
+                  </div>
+                
+              </div>
+            </div>
+            <div class="mdl-cell mdl-cell--2-col mdl-cell--1-col-phone">
+              <div class="mdl-grid">
+                <div class="mdl-cell mdl-cell--2-col mdl-cell--4-col-phone">
+                  <p class="name-highlight close-text">#2</p>
+                </div>
+                <div class="mdl-cell mdl-cell--2-col mdl-cell--4-col-phone">
+                   <p class="close-text">612</p>
+                </div>
+              </div>
+            </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="full-width-card mdl-cell mdl-cell--2-col mdl-cell--4-col-phone">
+        <div class="bronze-bg unset-height demo-card-square mdl-card mdl-shadow--2dp">
+          <div class="mdl-cell mdl-cell--2-col mdl-cell--4-col-phone">
+            <div class="mdl-grid">
+              <div class="mdl-cell mdl-cell--2-col mdl-cell--1-col-phone">
+                <img class="hunna-width" src="images/Avatar3.png">
+              </div>
+              <div class="mdl-cell mdl-cell--2-col mdl-cell--2-col-phone">
+                <div class="mdl-grid">
+                  <div class="mdl-cell mdl-cell--2-col mdl-cell--4-col-phone">
+                    <p class="name-highlight close-text">Simoen Smits</p>
+                  </div>
+                  <div class="mdl-cell mdl-cell--2-col mdl-cell--4-col-phone">
+                    <p class="close-text">Schipluiden</p>
+                  </div>
+                
+              </div>
+            </div>
+            <div class="mdl-cell mdl-cell--2-col mdl-cell--1-col-phone">
+              <div class="mdl-grid">
+                <div class="mdl-cell mdl-cell--2-col mdl-cell--4-col-phone">
+                  <p class="name-highlight close-text">#3</p>
+                </div>
+                <div class="mdl-cell mdl-cell--2-col mdl-cell--4-col-phone">
+                   <p class="close-text">522</p>
+                </div>
+              </div>
+            </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="full-width-card mdl-cell mdl-cell--2-col mdl-cell--4-col-phone">
+        <div class="unset-height demo-card-square mdl-card mdl-shadow--2dp">
+          <div class="mdl-cell mdl-cell--2-col mdl-cell--4-col-phone">
+            <div class="mdl-grid">
+              <div class="mdl-cell mdl-cell--2-col mdl-cell--1-col-phone">
+                <img class="hunna-width" src="images/Avatar4.png">
+              </div>
+              <div class="mdl-cell mdl-cell--2-col mdl-cell--2-col-phone">
+                <div class="mdl-grid">
+                  <div class="mdl-cell mdl-cell--2-col mdl-cell--4-col-phone">
+                    <p class="name-highlight close-text">Jan Jansma</p>
+                  </div>
+                  <div class="mdl-cell mdl-cell--2-col mdl-cell--4-col-phone">
+                    <p class="close-text">Amsterdam</p>
+                  </div>
+                
+              </div>
+            </div>
+            <div class="mdl-cell mdl-cell--2-col mdl-cell--1-col-phone">
+              <div class="mdl-grid">
+                <div class="mdl-cell mdl-cell--2-col mdl-cell--4-col-phone">
+                  <p class="name-highlight close-text">#4</p>
+                </div>
+                <div class="mdl-cell mdl-cell--2-col mdl-cell--4-col-phone">
+                   <p class="close-text">480</p>
+                </div>
+              </div>
+            </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="full-width-card mdl-cell mdl-cell--2-col mdl-cell--4-col-phone">
+        <div class="unset-height demo-card-square mdl-card mdl-shadow--2dp">
+          <div class="mdl-cell mdl-cell--2-col mdl-cell--4-col-phone">
+            <div class="mdl-grid">
+              <div class="mdl-cell mdl-cell--2-col mdl-cell--1-col-phone">
+                <img class="hunna-width" src="images/Avatar6.png">
+              </div>
+              <div class="mdl-cell mdl-cell--2-col mdl-cell--2-col-phone">
+                <div class="mdl-grid">
+                  <div class="mdl-cell mdl-cell--2-col mdl-cell--4-col-phone">
+                    <p class="name-highlight close-text">Jahseh Onfroy</p>
+                  </div>
+                  <div class="mdl-cell mdl-cell--2-col mdl-cell--4-col-phone">
+                    <p class="close-text">Rotterdam</p>
+                  </div>
+                
+              </div>
+            </div>
+            <div class="mdl-cell mdl-cell--2-col mdl-cell--1-col-phone">
+              <div class="mdl-grid">
+                <div class="mdl-cell mdl-cell--2-col mdl-cell--4-col-phone">
+                  <p class="name-highlight close-text">#5</p>
+                </div>
+                <div class="mdl-cell mdl-cell--2-col mdl-cell--4-col-phone">
+                   <p class="close-text">444</p>
+                </div>
+              </div>
+            </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="full-width-card mdl-cell mdl-cell--2-col mdl-cell--4-col-phone">
+        <div class="unset-height demo-card-square mdl-card mdl-shadow--2dp">
+          <div class="mdl-cell mdl-cell--2-col mdl-cell--4-col-phone">
+            <div class="mdl-grid">
+              <div class="mdl-cell mdl-cell--2-col mdl-cell--1-col-phone">
+                <img class="hunna-width" src="images/Avatar7.png">
+              </div>
+              <div class="mdl-cell mdl-cell--2-col mdl-cell--2-col-phone">
+                <div class="mdl-grid">
+                  <div class="mdl-cell mdl-cell--2-col mdl-cell--4-col-phone">
+                    <p class="name-highlight close-text">Richard Milik</p>
+                  </div>
+                  <div class="mdl-cell mdl-cell--2-col mdl-cell--4-col-phone">
+                    <p class="close-text">Venlo</p>
+                  </div>
+                
+              </div>
+            </div>
+            <div class="mdl-cell mdl-cell--2-col mdl-cell--1-col-phone">
+              <div class="mdl-grid">
+                <div class="mdl-cell mdl-cell--2-col mdl-cell--4-col-phone">
+                  <p class="name-highlight close-text">#6</p>
+                </div>
+                <div class="mdl-cell mdl-cell--2-col mdl-cell--4-col-phone">
+                   <p class="close-text">390</p>
+                </div>
+              </div>
+            </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="full-width-card mdl-cell mdl-cell--2-col mdl-cell--4-col-phone">
+        <div class="unset-height demo-card-square mdl-card mdl-shadow--2dp">
+          <div class="mdl-cell mdl-cell--2-col mdl-cell--4-col-phone">
+            <div class="mdl-grid">
+              <div class="mdl-cell mdl-cell--2-col mdl-cell--1-col-phone">
+                <img class="hunna-width" src="images/Avatar8.png">
+              </div>
+              <div class="mdl-cell mdl-cell--2-col mdl-cell--2-col-phone">
+                <div class="mdl-grid">
+                  <div class="mdl-cell mdl-cell--2-col mdl-cell--4-col-phone">
+                    <p class="name-highlight close-text">Jeff de Jong</p>
+                  </div>
+                  <div class="mdl-cell mdl-cell--2-col mdl-cell--4-col-phone">
+                    <p class="close-text">Vlissingen</p>
+                  </div>
+                
+              </div>
+            </div>
+            <div class="mdl-cell mdl-cell--2-col mdl-cell--1-col-phone">
+              <div class="mdl-grid">
+                <div class="mdl-cell mdl-cell--2-col mdl-cell--4-col-phone">
+                  <p class="name-highlight close-text">#7</p>
+                </div>
+                <div class="mdl-cell mdl-cell--2-col mdl-cell--4-col-phone">
+                   <p class="close-text">360</p>
+                </div>
+              </div>
+            </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="full-width-card mdl-cell mdl-cell--2-col mdl-cell--4-col-phone">
+        <div class="unset-height demo-card-square mdl-card mdl-shadow--2dp">
+          <div class="mdl-cell mdl-cell--2-col mdl-cell--4-col-phone">
+            <div class="mdl-grid">
+              <div class="mdl-cell mdl-cell--2-col mdl-cell--1-col-phone">
+                <img class="hunna-width" src="images/Avatar5.png">
+              </div>
+              <div class="mdl-cell mdl-cell--2-col mdl-cell--2-col-phone">
+                <div class="mdl-grid">
+                  <div class="mdl-cell mdl-cell--2-col mdl-cell--4-col-phone">
+                    <p class="name-highlight close-text">Alex Akema</p>
+                  </div>
+                  <div class="mdl-cell mdl-cell--2-col mdl-cell--4-col-phone">
+                    <p class="close-text">Amsterdam</p>
+                  </div>
+                
+              </div>
+            </div>
+            <div class="mdl-cell mdl-cell--2-col mdl-cell--1-col-phone">
+              <div class="mdl-grid">
+                <div class="mdl-cell mdl-cell--2-col mdl-cell--4-col-phone">
+                  <p class="name-highlight close-text">#8</p>
+                </div>
+                <div class="mdl-cell mdl-cell--2-col mdl-cell--4-col-phone">
+                   <p class="close-text">333</p>
+                </div>
+              </div>
+            </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+  </div>
+  </main>
+</div>
+
+<script src="javascript/material.min.js"></script>
+<script src="javascript/progressbar.min.js"></script>
+<script src="javascript/progress.js"></script>
+</body>
+</html>
